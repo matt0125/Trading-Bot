ae1fecea3bcda063b68ec2b17329d3faaea74cb8
Matthew Daley <m.daley0125@gmail.com>
2023-09-30 20:45:15 -0400

    JSONify
    Stores variables in vars.json and api details in config.json
    This is to allow the project to be ran as a script once a day without losing information

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import alpaca_trade_api as tradeapi
 import json
 
-class Alpaca:
-    def __init__(self, configFile="config.json"):
 
-        with open(configFile, 'r') as config_file:
-            self.config = json.load(config_file)
-
-        self.api_key = self.config["api_key"]
-        self.api_secret = self.config["api_secret"]
-        self.short_window = self.config["short_window"]
-        self.long_window = self.config["long_window"]
-        self.polling_interval_seconds = self.config["polling_interval_seconds"]
-        self.base_url = self.config["base_url"]
-
-        self.api = tradeapi.REST(self.api_key, self.api_secret, base_url = self.base_url)
-        print("a")
-
-
-    def placeOrder(self, symbol, qty):
-
-        order = self.api.submit_order(
-            symbol=symbol,
-            qty=qty,
-            side='buy',
-            type='market',
-            time_in_force='gtc'
-        )
-
-        return order
+class TradingBot():
     
-    def getEquity(self):
-        print(self.api.get_account().equity)
+    def __init__(self, configFile, varsFile):
+        self.varFile = varsFile
+        self.ReadVars()
+        self.ConfigAPI(configFile)
 
-# Usage:
-alpaca = Alpaca()  # Load configuration from "config.json" by default
+    def ReadVars(self):
+        with open(self.varFile, "r") as varsFile:
+            vars = json.load(varsFile)
 
-alpaca.getEquity()
+        # Extract configuration values
+        self.symbol = vars["symbol"]
+        self.lookback = vars["lookback"]
+        self.upperLookback = vars["lookback_upperLimit"]
+        self.lowerLookback = vars["lookback_lowerLimit"]
+        self.initialStopRisk = vars["stopRisk_initial"]
+        self.trailingStopRisk = vars["stopRisk_trailing"]
+
+    def ConfigAPI(self, configFile):
+        # Load configuration from config.json
+        with open(configFile, "r") as config_file:
+            config = json.load(config_file)
+        
+        # Alpaca API credentials
+        base_url = config["base_url_paper"]
+        api_key = config["api_key_paper"]
+        api_secret = config["api_secret_paper"]
+
+        # Initialize Alpaca API
+        self.api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+
+    def UpdateVars(self):
+        
+        vars = {
+            "symbol": self.symbol,
+            "lookback": self.lookback,
+            "lookback_upperLimit": self.upperLookback,
+            "lookback_lowerLimit": self.lowerLookback,
+            "stopRisk_initial": self.initialStopRisk,
+            "stopRisk_trailing": self.trailingStopRisk
+        }
+
+        with open(self.varFile, "w") as varsFile:
+            json.dump(vars, varsFile, indent=4)
+
+bot = TradingBot(configFile="config.json", varsFile="vars.json")
+
+
+bot.UpdateVars()
